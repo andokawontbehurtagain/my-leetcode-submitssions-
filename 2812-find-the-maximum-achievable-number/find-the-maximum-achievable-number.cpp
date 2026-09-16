e6cf8aef9fbba8b1a0e3938a9c4aad713643fa67
andoka <xtalefrisk2206@gmail.com>
2026-09-16 19:11:07 +0700

    Time: 0 ms (100.00%) | Memory: 9.9 MB (0.03%) - LeetSync

---
+++ 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+         ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int ans=num+t*2;
+        return ans;
+    }
+};
