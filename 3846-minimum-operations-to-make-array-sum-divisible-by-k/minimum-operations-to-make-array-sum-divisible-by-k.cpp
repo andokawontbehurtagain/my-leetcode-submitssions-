a62a8a692cbef597fa5a09052622838b768a7079
andoka <xtalefrisk2206@gmail.com>
2026-09-16 19:01:16 +0700

    Time: 0 ms (100.00%) | Memory: 45.6 MB (0.40%) - LeetSync

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
+         ios_base::sync_with_stdio(false);
+         cin.tie(NULL);
         int sum=0;
         for(int i=0;i<=nums.size()-1;i++){
             sum+=nums[i];
